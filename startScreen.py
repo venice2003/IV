6a92b039d1d4b20206f984ae7736ec9c5f881ea3
iceCream <60511119+Blueguy0301@users.noreply.github.com>
2023-06-16 23:58:23 +0800

    added medicalHistory UI

--- startScreen.py
+++ startScreen.py
@@ -1,50 +1,47 @@
-from PyQt6.QtCore import Qt,QEvent,QObject
-from PyQt6.QtWidgets import  QMainWindow,  QStackedWidget, QPushButton,QLineEdit,QFrame,QTextEdit,QApplication
+from PyQt6.QtWidgets import  QMainWindow,  QStackedWidget, QPushButton,QLineEdit,QFrame,QTextEdit,QCheckBox
 from PyQt6 import uic
-
+resetObject = "reset"
+nextObject = "next"
+backObject = "back"
+nextEnd = "nextEnd"
+formFrame ="formFrame"
+files = ['./Screens/mainWindow.ui', './Screens/emergencyScreen.ui', './Screens/medicHistory.ui']
+uiObjects = []
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle('Student Medical Records')
-        self.setGeometry(800, 300, 500, 500)
+        self.setGeometry(0, 00, 1024, 768)
         self.stacked_widget = QStackedWidget()
         self.setCentralWidget(self.stacked_widget)
+        # List of UI file paths
 
-        # Load the UI files
-        self.firstUI = uic.loadUi('./Screens/mainWindow.ui')
-        self.secondUI = uic.loadUi('./Screens/emergencyScreen.ui')
-
-        # Add the UI files to the stacked widget
-        self.stacked_widget.addWidget(self.firstUI)
-        self.stacked_widget.addWidget(self.secondUI)
-
-        nextButton = self.firstUI.findChild(QPushButton, 'welcomeNext')
-        nextButton.clicked.connect(self.next)
-
-        backButton = self.secondUI.findChild(QPushButton, 'consultBack')
-        backButton.clicked.connect(self.back)
-
-        reset_button = self.firstUI.findChild(QPushButton, 'resetButton')
-        reset_button1 = self.secondUI.findChild(QPushButton, 'resetButton')
-        reset_button.clicked.connect(lambda: self.resetForm("welcomeForm"))
-        reset_button1.clicked.connect(lambda: self.resetForm("consultForm"))
-
-
-        # Connect the clicked signal of the reset button to the reset_form function
-    def resetForm(self,frameName):
-        print(frameName)
-        # Find the frame in the UI
-        frame = self.findChild(QFrame, frameName)
-
+        for filePath in files:
+            ui = uic.loadUi(filePath)
+            uiObjects.append(ui)
+            self.stacked_widget.addWidget(ui)
+        for index, ui in enumerate(uiObjects):
+            nextButton = ui.findChild(QPushButton, nextObject)
+            if nextButton : nextButton.clicked.connect(self.next)
+            print(index)
+            reset_button = ui.findChild(QPushButton, resetObject)
+            reset_button.clicked.connect(lambda checked, ui=ui: self.resetForm(checked,ui))
+            backButton = ui.findChild(QPushButton, backObject)
+            backButton.clicked.connect(self.back)
+            if index ==len(uiObjects):  
+                nextButton = ui.findChild(QPushButton, nextEnd)
+                nextButton.clicked.connect(self.submit)
+    def resetForm(self, checked, ui):
+        frame = ui.findChild(QFrame, formFrame)
         if frame is not None:
             # Iterate over the child widgets of the frame
-            for widget in frame.findChildren(QLineEdit)+frame.findChildren(QTextEdit):
-                if isinstance(widget, QLineEdit):
-                    # Clear the text of QLineEdit widgets
+            for widget in frame.findChildren(QLineEdit) + frame.findChildren(QTextEdit) + frame.findChildren(QCheckBox):
+                if isinstance(widget, QLineEdit) or isinstance(widget, QTextEdit):
+                    # Clear the text of QLineEdit and QTextEdit widgets
                     widget.clear()
-                if isinstance(widget, QTextEdit):
-                    # Clear the text of QLineEdit widgets
-                    widget.clear()
+                if isinstance(widget, QCheckBox):
+                    # Uncheck the checkboxes
+                    widget.setChecked(False)
     def next(self):
         # Change the current index of the QStackedWidget
         current_index = self.stacked_widget.currentIndex()
